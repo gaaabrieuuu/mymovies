7b1443eab71a3de0288ab8788a2aca81c59e4c0c
gaaabrieuuu <gabriel.antonio2005@outlook.com>
2024-06-29 21:02:35 -0300

    T001: build - Criação básica dos componentes Header, Card e Cards List

---
+++ src/app/components/cards-list/cards-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cards-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cards-list.component.html',
+  styleUrls: ['./cards-list.component.scss']
+})
+export class CardsListComponent {
+
+}
