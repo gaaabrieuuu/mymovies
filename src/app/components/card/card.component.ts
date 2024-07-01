e567e4b75c655a9da1fd039edc3ed77822741a4a
gaaabrieuuu <gabriel.antonio2005@outlook.com>
2024-07-01 12:21:32 -0300

    M003: build - Término do Card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Show } from 'src/app/models/show';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-
+ @Input() show: Show | undefined;
 }
