6b8ac05568fa6b2228f93c158371ced405328527
gaaabrieuuu <gabriel.antonio2005@outlook.com>
2024-07-01 12:30:35 -0300

    M003: build - Implementação do CardsList na home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Show } from 'src/app/models/show';
+import { TMDBService } from 'src/app/services/tmdb.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  movies: Show[] = [];
+  tvShows: Show[] = [];
+  constructor(private readonly service: TMDBService) {}
 
+  ngOnInit(): void {
+    this.getMovies();
+    this.getTvShows();
+  }
+
+  getMovies = () => {
+    this.service.searchMovies().subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.movies.push(...data);
+      },
+      error: (e: any) => console.error(e),
+    });
+  };
+
+  getTvShows = () => {
+    this.service.searchTvShows().subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.tvShows.push(...data)
+      },
+      error: (e: any) => console.error(e),
+    });
+  };
 }
