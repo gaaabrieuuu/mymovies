b067301ad0a31ca02520b51765794258a818ad8d
gaaabrieuuu <gabriel.antonio2005@outlook.com>
2024-07-01 12:37:36 -0300

    test

--- src/app/services/tmdb.service.ts
+++ src/app/services/tmdb.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 import { Observable, map } from 'rxjs';
 import { Show } from '../models/show';
 
